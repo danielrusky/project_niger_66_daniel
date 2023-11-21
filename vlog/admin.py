9f66b85125afdd1dfdfb7e6eb636df6b1026efa8
Danila Kharchenko <spook9400@mail.ru>
2023-11-21 15:56:45 +0300

    add homework 24.2

--- vlog/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from vlog.models import Record
-
-admin.site.register(Record)
--- vlog/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from vlog.models import Record
-from mailing_list.forms import StyleFormMixin
-
-
-class RecordForm(StyleFormMixin, forms.ModelForm):
-    class Meta:
-        model = Record
-        fields = ('title', 'slug', 'content', 'preview')
